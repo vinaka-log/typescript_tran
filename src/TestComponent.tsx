d7e6171a5b0a4d761360febd09bc0e7c52bca277
vinaka <k64.yuta29@gmail.com>
2021-07-10 12:20:13 +0900

    useState

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -4,11 +4,19 @@ interface props {
     text: string
 }
 
+interface UserData {
+    id: number;
+    name: string;
+}
+
 const TestComponent: React.FC<props> = (props) => {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState<number | null>(12);
+    const [user, setuser] = useState<UserData>({id:1, name: "dummy"});
+    // setCount(1)
     return (
         <div>
             <h1>{props.text}</h1>
+            <h1>{count}</h1>
         </div>
     )
 }
