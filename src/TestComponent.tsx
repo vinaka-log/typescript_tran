dfddca51def7630fe036595c2a492ae7700efe9f
vinaka <k64.yuta29@gmail.com>
2021-07-10 12:32:29 +0900

    データ型

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -12,11 +12,16 @@ interface UserData {
 const TestComponent: React.FC<props> = (props) => {
     const [count, setCount] = useState<number | null>(12);
     const [user, setuser] = useState<UserData>({id:1, name: "dummy"});
+    const [inputData, setInputData] = useState("");
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+      setInputData(e.target.value);
     // setCount(1)
     return (
         <div>
             <h1>{props.text}</h1>
             <h1>{count}</h1>
+            <input type="text" value={inputData} onChange={handleInputChange} />
+            <h1>{inputData}</h1>
         </div>
     )
 }
