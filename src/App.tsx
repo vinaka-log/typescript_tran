a04fc507f1ea8647c703334b9bc1566dca700108
vinaka <k64.yuta29@gmail.com>
2021-07-10 10:38:21 +0900

    mリテラル型とオブジェクト型

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const name  = "hello";
+
+リテラル型
+// name = "hello";
+
+let nameChange  = "hello";
+nameChange = "hello2";
+
+let username:string = "Hello";
+let dummyNomber:number = 2;
+let bool:boolean = true;
+
+let array1 = [true, false,true];
+let array2 = [0, 1,"Hello"];
+
+interface NAME {
+  first: string;
+  last?: string;
+}
+
+let nameObj: NAME = {first: "Yamada" };
+
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
     </div>
   );
 }
